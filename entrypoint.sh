fd7b11b5903e376d15172d3fc00257a574cd458d
purna_reddy <purna@pingbase.ai>
2024-05-08 15:41:14 +0530

    purna: updated entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 set -e
 
 if [ "$MODE" = "worker" ]; then
-    exec NEW_RELIC_CONFIG_FILE=newrelic.ini newrelic-admin run-program python manage.py qcluster
+    exec  newrelic-admin run-program python manage.py qcluster
 else
-    exec NEW_RELIC_CONFIG_FILE=newrelic.ini newrelic-admin run-program gunicorn --bind 0.0.0.0:8000 master_mind.wsgi:application --timeout 240
+    exec  newrelic-admin run-program gunicorn --bind 0.0.0.0:8000 master_mind.wsgi:application --timeout 240
 fi
