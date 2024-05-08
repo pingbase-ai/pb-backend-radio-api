d0eaf4e25a71dfe162a634137a51bc2914a86abc
purna_reddy <purna@pingbase.ai>
2024-05-08 15:35:29 +0530

    purna: added new relic integration

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 set -e
 
 if [ "$MODE" = "worker" ]; then
-    exec python manage.py qcluster
+    exec NEW_RELIC_CONFIG_FILE=newrelic.ini newrelic-admin run-program python manage.py qcluster
 else
-    exec gunicorn --bind 0.0.0.0:8000 master_mind.wsgi:application --timeout 240
+    exec NEW_RELIC_CONFIG_FILE=newrelic.ini newrelic-admin run-program gunicorn --bind 0.0.0.0:8000 master_mind.wsgi:application --timeout 240
 fi
