bb94a7584699b7653a11497c370cb1f2cb78790e
purna_reddy <purna.reddy@spoonshot.com>
2024-07-02 11:40:26 +0530

    purna: slack notification fix

--- master_mind/settings.py
+++ master_mind/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-VERSION = "1.0.16"
+VERSION = "1.0.17"
 
 ALLOWED_HOSTS = ["*"]
 
