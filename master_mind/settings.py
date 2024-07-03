d36c57113b9096bf977f8ee335475b0065c3da9b
purna_reddy <purna.reddy@spoonshot.com>
2024-07-03 12:28:47 +0530

    purna: bump version

--- master_mind/settings.py
+++ master_mind/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-VERSION = "1.0.18"
+VERSION = "1.0.19"
 
 ALLOWED_HOSTS = ["*"]
 
